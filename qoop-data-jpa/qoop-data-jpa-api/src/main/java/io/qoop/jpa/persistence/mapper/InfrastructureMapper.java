9d179a7294c684dd38bab7aa42797a5370dcce84
d_akbari <daak1365@gmail.com>
2026-02-26 08:51:38 +0330

    InfrastructureMapper-Optional ...

--- qoop-data-jpa/qoop-data-jpa-api/src/main/java/io/qoop/jpa/persistence/mapper/InfrastructureMapper.java
+++ qoop-data-jpa/qoop-data-jpa-api/src/main/java/io/qoop/jpa/persistence/mapper/InfrastructureMapper.java
@@ -1,5 +1,7 @@
 package io.qoop.jpa.persistence.mapper;
 
+import java.util.Optional;
+
 /**
  * Author: davood akbari
  * Email: daak1365@gmail.com
@@ -12,4 +14,14 @@ public interface InfrastructureMapper<D, E> {
     D toDomain(E e);
 
     E toEntity(D d);
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    default Optional<D> toDomain(Optional<E> e) {
+        return e.map(this::toDomain);
+    }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    default Optional<E> toEntity(Optional<D> e) {
+        return e.map(this::toEntity);
+    }
 }
